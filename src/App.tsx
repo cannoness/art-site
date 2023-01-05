3183eb04234d7205abfce17fee5aa3cd7d03a8c0
KASSUR\Kadyn <kascopia@gmail.com>
2023-01-04 20:23:14 -0700

    add packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import AbcIcon from '@mui/icons-material/Abc';
+
 const App: React.FC = ()=> {
   return (
-    <>Hello, ThumbHub!</>
+    <>Hello, ThumbHub!
+    <AbcIcon/>
+    </>
   );
 }
 
